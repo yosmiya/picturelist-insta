b134404308932644e61dcb92279b046b05f5df35
yosmiya <gigongben@gmail.com>
2020-08-03 11:33:30 +0900

    Updates

---
+++ precache-manifest.8338e2d726b38a63c03c60e54c70b327.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77b586e38340c5e92501f2a513daabb1",
+    "url": "/picturelist-insta/index.html"
+  },
+  {
+    "revision": "5f2a89048a94a8787464",
+    "url": "/picturelist-insta/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6578ecb508199a318d72",
+    "url": "/picturelist-insta/static/js/2.37ba42c1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/picturelist-insta/static/js/2.37ba42c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f2a89048a94a8787464",
+    "url": "/picturelist-insta/static/js/main.9f4f677f.chunk.js"
+  },
+  {
+    "revision": "ecb4115c25a4705fce6b",
+    "url": "/picturelist-insta/static/js/runtime-main.87aedee3.js"
+  }
+]);
